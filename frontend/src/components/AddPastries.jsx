3b571eb7d8732645303fa07f62c2bdfee276fdbb
MarineDeveza <marine.deveza@gmail.com>
2023-07-17 18:27:40 +0200

    addpastriescards-css

--- frontend/src/components/AddPastries.jsx
+++ frontend/src/components/AddPastries.jsx
@@ -2,6 +2,7 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
 import Swal from "sweetalert2";
+import "../AddPastries.css";
 
 function AddPastries() {
   const [reference, setReference] = useState("");
